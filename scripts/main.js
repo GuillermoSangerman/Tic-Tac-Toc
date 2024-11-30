41e53d6df6e5d2c0e0a1a3066b7142ce67a6f90f
Guillermo Sangerman <sangermanmptn8@gmail.com>
2024-11-29 18:08:36 -0600

    edite mi modal para que no se pegue

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,7 @@
   scoreTies.innerHTML = 0;
   let playerOne = true;
   let score = { X: 0, O: 0, ties: 0 }
+  let pc = aleatorio(0, 9)
   for (let i = 0; i < 9; i++) {
     boardItem[i].addEventListener('click', jugadorQueMueve);
   }
@@ -22,24 +23,27 @@
     if (!movimiento.length) {
       e.target.innerHTML = playerOne ? `<svg class="icon cross">
             <use xlink:href="./icons/icon-x.svg#icon-x"></use>
-          </svg>` : `<svg class="icon circle">
+          </svg>` :  `<svg class="icon circle">
             <use xlink:href="./icons/icon-o.svg#icon-o"></use>
           </svg>`;
       playerOne = !playerOne;
       turn.innerHTML = playerOne ? `<use xlink:href="./icons/icon-x.svg#icon-x"></use>` 
       : `<use xlink:href="./icons/icon-o.svg#icon-o"></use>`
-      combinacionesAGanar(0, 1, 2, playerOne);
-      combinacionesAGanar(3, 4, 5, playerOne);
-      combinacionesAGanar(6, 7, 8, playerOne);
-      combinacionesAGanar(0, 3, 6, playerOne);
-      combinacionesAGanar(1, 4, 7, playerOne);
-      combinacionesAGanar(2, 5, 8, playerOne);
-      combinacionesAGanar(0, 4, 8, playerOne);
-      combinacionesAGanar(2, 4, 6, playerOne);
-      
     }
+    
+    
+      combinacionesAGanar(0, 1, 2, playerOne,pc);
+      combinacionesAGanar(3, 4, 5, playerOne,pc);
+      combinacionesAGanar(6, 7, 8, playerOne,pc);
+      combinacionesAGanar(0, 3, 6, playerOne,pc);
+      combinacionesAGanar(1, 4, 7, playerOne,pc);
+      combinacionesAGanar(2, 5, 8, playerOne,pc);
+      combinacionesAGanar(0, 4, 8, playerOne,pc);
+      combinacionesAGanar(2, 4, 6, playerOne,pc);
+      
+    
   }
-  function combinacionesAGanar(celda1, celda2, celda3, playerOne) {
+  function combinacionesAGanar(celda1, celda2, celda3, playerOne,pc) {
     if (boardItem[celda1].innerHTML.length &&
       boardItem[celda1].innerHTML === boardItem[celda2].innerHTML &&
       boardItem[celda2].innerHTML === boardItem[celda3].innerHTML) {
@@ -47,6 +51,9 @@
       
       console.log("ganador :", celda1, celda2, celda3, playerOne);
     }
+}
+function aleatorio(min, max){
+  return Math.floor(Math.random() * (max - min ) + min)
 }
 
   function toggleModal() {
@@ -57,12 +64,14 @@
     modal.style = ` display:block !important;
                     place-content: center !important;`
     if (ganador === false) {
-      document.querySelector('#winner use').innerHTML = `<use xlink:href="./icons/icon-x.svg#icon-x"></use>`
+      document.querySelector('#winner').innerHTML = `<use xlink:href="./icons/icon-x.svg#icon-x"></use>`
       score.X++;
       updateScores();
+    
     } else if (ganador === true) {
       document.querySelector('#winner').innerHTML = `<use xlink:href="./icons/icon-o.svg#icon-o"></use>`
       score.O++;
+   
       updateScores();
     }
   }
