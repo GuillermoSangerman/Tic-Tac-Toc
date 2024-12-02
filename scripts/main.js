b1b0bfc2db7092d0f0b100f82abeb8019989fdd9
Guillermo Sangerman <sangermanmptn8@gmail.com>
2024-12-01 20:27:50 -0600

    funcional

--- scripts/main.js
+++ scripts/main.js
@@ -1,111 +1,126 @@
+const restart = document.querySelector('#restart')
+const scoreX = document.querySelector('#score')
+const scoreTies = document.querySelector('#score1')
+const scoreO = document.querySelector('#score2')
+const quit = document.querySelector('#quit')
+const nextRound = document.querySelector('#nextRound')
+const modal = document.querySelector('.modal')
+const turn = document.querySelector('.turn .icon')
+const boardItem = document.querySelectorAll('.board_item')
+scoreO.innerHTML = 0;
+scoreX.innerHTML = 0;
+scoreTies.innerHTML = 0;
+let playerOne = 0;
+let score = { X: 0, O: 0, ties: 0 }
+let pc = 1;
+let juegoActivo = false ;
+let juegoEnPause = false ;
+turn.innerHTML =  `<use xlink:href="./icons/icon-x.svg#icon-x"></use>`
 
-  const restart = document.querySelector('#restart')
-  const scoreX = document.querySelector('#score')
-  const scoreTies = document.querySelector('#score1')
-  const scoreO = document.querySelector('#score2')
-  const quit = document.querySelector('#quit')
-  const nextRound = document.querySelector('#nextRound')
-  const modal = document.querySelector('.modal')
-  const turn = document.querySelector('.turn .icon')
-  const boardItem = document.querySelectorAll('.board_item')
-  scoreO.innerHTML = 0;
-  scoreX.innerHTML = 0;
-  scoreTies.innerHTML = 0;
-  let playerOne = true;
-  let score = { X: 0, O: 0, ties: 0 }
-  let pc = aleatorio(0, 9)
-  for (let i = 0; i < 9; i++) {
-    boardItem[i].addEventListener('click', jugadorQueMueve);
-  }
-  function jugadorQueMueve(e) {
-    let movimiento = e.target.innerHTML;
-    console.log(movimiento);
-    if (!movimiento.length) {
-      e.target.innerHTML = playerOne ? `<svg class="icon cross">
+for (let i = 0; i < 9; i++) {
+  boardItem[i].addEventListener('click', jugadorQueMueve);
+}
+function jugadorQueMueve(e) {
+  let movimiento = e.target.innerHTML;
+  if (!movimiento.length && !juegoEnPause) {
+    juegoActivo = true;
+    e.target.innerHTML = playerOne = `<svg class="icon cross">
             <use xlink:href="./icons/icon-x.svg#icon-x"></use>
-          </svg>` :  `<svg class="icon circle">
-            <use xlink:href="./icons/icon-o.svg#icon-o"></use>
-          </svg>`;
-      playerOne = !playerOne;
-      turn.innerHTML = playerOne ? `<use xlink:href="./icons/icon-x.svg#icon-x"></use>` 
-      : `<use xlink:href="./icons/icon-o.svg#icon-o"></use>`
-    }
-    
-    
-      combinacionesAGanar(0, 1, 2, playerOne,pc);
-      combinacionesAGanar(3, 4, 5, playerOne,pc);
-      combinacionesAGanar(6, 7, 8, playerOne,pc);
-      combinacionesAGanar(0, 3, 6, playerOne,pc);
-      combinacionesAGanar(1, 4, 7, playerOne,pc);
-      combinacionesAGanar(2, 5, 8, playerOne,pc);
-      combinacionesAGanar(0, 4, 8, playerOne,pc);
-      combinacionesAGanar(2, 4, 6, playerOne,pc);
-      
-    
+          </svg>`
+    playerOne = 0;
+    turn.innerHTML = `<use xlink:href="./icons/icon-o.svg#icon-o"></use>`
+    combinacionesAGanar(0, 1, 2, playerOne),
+    combinacionesAGanar(3, 4, 5, playerOne),
+    combinacionesAGanar(6, 7, 8, playerOne),
+    combinacionesAGanar(0, 3, 6, playerOne),
+    combinacionesAGanar(1, 4, 7, playerOne),
+    combinacionesAGanar(2, 5, 8, playerOne),
+    combinacionesAGanar(0, 4, 8, playerOne),
+    combinacionesAGanar(2, 4, 6, playerOne)
   }
-  function combinacionesAGanar(celda1, celda2, celda3, playerOne,pc) {
-    if (boardItem[celda1].innerHTML.length &&
-      boardItem[celda1].innerHTML === boardItem[celda2].innerHTML &&
-      boardItem[celda2].innerHTML === boardItem[celda3].innerHTML) {
-      ganadorModal(playerOne);
-      
-      console.log("ganador :", celda1, celda2, celda3, playerOne);
-    }
+  setTimeout(() => {
+    let nuevoArray = Array.from(boardItem).filter(celdas => !celdas.innerHTML.length);
+    if (nuevoArray.length > 1 && !juegoEnPause) {
+      juegoActivo = true;
+      let celdaRandom = nuevoArray[Math.floor(Math.random() * nuevoArray.length)];
+      celdaRandom.innerHTML = `<svg class="icon circle"><use xlink:href="./icons/icon-o.svg#icon-o"></use></svg>`;
+      pc = 1;
+      turn.innerHTML = `<use xlink:href="./icons/icon-x.svg#icon-x"></use>`
+        combinacionesAGanar(0, 1, 2, pc),
+        combinacionesAGanar(3, 4, 5, pc),
+        combinacionesAGanar(6, 7, 8, pc),
+        combinacionesAGanar(0, 3, 6, pc),
+        combinacionesAGanar(1, 4, 7, pc),
+        combinacionesAGanar(2, 5, 8, pc),
+        combinacionesAGanar(0, 4, 8, pc),
+        combinacionesAGanar(2, 4, 6, pc)
+    }else if (nuevoArray.length === 0 && juegoEnPause === false) {
+       empate()
+       turn.innerHTML = `<use xlink:href="./icons/icon-x.svg#icon-x"></use>`
+     }
+  }, "400");
 }
-function aleatorio(min, max){
-  return Math.floor(Math.random() * (max - min ) + min)
-}
-
-  function toggleModal() {
-    modal.classList.toggle('d-none')
+function combinacionesAGanar(celda1, celda2, celda3, ganador) {
+  if (boardItem[celda1].innerHTML.length && boardItem[celda1].innerHTML === boardItem[celda2].innerHTML && boardItem[celda2].innerHTML === boardItem[celda3].innerHTML) {
+    ganadorModal(ganador);
+    return true
   }
-  const winnerIcon = document.getElementById("winner")
-  function ganadorModal(ganador) {
+}
+function ganadorModal(modalElGanador) {
+  modal.style = ` display:block !important;
+                    place-content: center !important;`
+  if (modalElGanador === 0) {
+    document.querySelector('.modal h6').textContent = "YOU WON!"; 
+    document.querySelector('.modal h2').textContent = "TAKES THE ROUND"; 
+    document.querySelector('#winner').innerHTML = `<use xlink:href="./icons/icon-x.svg#icon-x"></use>`
+    score.X++;
+    updateScores();
+    juegoEnPause = true
+  } else if (modalElGanador === 1) {
+    document.querySelector('.modal h6').textContent = "YOU WON!"; 
+    document.querySelector('.modal h2').textContent = "TAKES THE ROUND"; 
+    document.querySelector('#winner').innerHTML = `<use xlink:href="./icons/icon-o.svg#icon-o"></use>`
+    score.O++;
+    updateScores();
+  }
+}
+function empate() {
     modal.style = ` display:block !important;
                     place-content: center !important;`
-    if (ganador === false) {
-      document.querySelector('#winner').innerHTML = `<use xlink:href="./icons/icon-x.svg#icon-x"></use>`
-      score.X++;
-      updateScores();
-    
-    } else if (ganador === true) {
-      document.querySelector('#winner').innerHTML = `<use xlink:href="./icons/icon-o.svg#icon-o"></use>`
-      score.O++;
-   
-      updateScores();
-    }
+    document.querySelector('.modal h2').textContent = "IT'S A TIE!"; 
+    document.querySelector('#winner').innerHTML = `<use xlink:href="./icons/icon-x.svg#icon-x"> <use xlink:href="./icons/icon-o.svg#icon-o"></use>`; 
+    score.ties++;
+    updateScores()
   }
-
-  quit.addEventListener('click', () => {
-    modal.style = " display:none !important;"
-    toggleModal()
-    limpiarTablero()
-    scoreX.innerText = 0
-    scoreTies.innerText = 0
-    scoreO.innerText = 0
-  })
-
-  function limpiarTablero() {
-    boardItem.forEach((element) => {
-      element.innerHTML = ""
-    })
-  }
-
+quit.addEventListener('click', () => {
+  modal.style = " display:none !important;"
   limpiarTablero()
-  restart.addEventListener('click', function () {
-    limpiarTablero()
+  scoreX.innerText = 0
+  scoreTies.innerText = 0
+  scoreO.innerText = 0
+  score = { X: 0, O: 0, ties: 0 }
+  juegoActivo = false
+  juegoEnPause = false
+})
+function limpiarTablero() {
+  boardItem.forEach((element) => {
+    element.innerHTML = ""
   })
-  nextRound.addEventListener('click', () => {
-    modal.style = " display:none !important;"
-    limpiarTablero()
-  })
+}
+limpiarTablero()
+restart.addEventListener('click', function () {
+  limpiarTablero()
+})
+nextRound.addEventListener('click', () => {
+  modal.style = " display:none !important;"
+  limpiarTablero()
+  turn.innerHTML =  `<use xlink:href="./icons/icon-x.svg#icon-x"></use>`
+  juegoActivo = false
+  juegoEnPause = false
+})
+const updateScores = () => {
+  scoreX.innerHTML = score.X;
+  scoreO.innerHTML = score.O;
+  scoreTies.innerHTML = score.ties;
 
-
-  const updateScores = () => {
-    scoreX.innerHTML = score.X;
-    scoreO.innerHTML = score.O;
-    scoreTies.innerHTML = score.ties;
-   console.log(scoreTies.innerHTML);
-  };
-
-
+};
